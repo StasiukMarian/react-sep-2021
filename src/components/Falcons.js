3fd5df6f79b9fb41a97640091ccfec972aa352d0
StasiukMarian <lilmaryan2001@gmail.com>
2022-01-12 21:50:12 +0200

    changed cw1

--- src/components/Falcons.js
+++ src/components/Falcons.js
@@ -4,18 +4,18 @@ import Falcon from "./Falcon";
 const Falcons = () => {
     let [launch , setLaunch] = useState([])
     useEffect(() => {
-       fetch('https://api.spacexdata.com/v3/launches/')
-           .then(value => value.json())
-           .then(value => setLaunch(value))
-    } , [])
+        fetch('https://api.spacexdata.com/v3/launches/')
+            .then(value => value.json())
+            .then(value => {
+                setLaunch(value.filter(value => value.launch_year !== '2020'))
+            })
+            }, [])
 
     return (
          <div className='falcon-wrap'>
-             {launch.map(value => {
-                 if (value.launch_year !== '2020') {
+             {launch.map(value =>{
                     return (<Falcon mission_name={value.mission_name} launch_year={value.launch_year}
                     mission_patch_small={value.links.mission_patch_small} />)}
-             }
              )}
         </div>
     );
