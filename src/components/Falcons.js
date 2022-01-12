e4f17027e1e456ff9675d91dfc5d8b5e23784c35
StasiukMarian <lilmaryan2001@gmail.com>
2022-01-12 15:02:28 +0200

    created cw1 on branch cw1

--- src/components/Falcons.js
+++ src/components/Falcons.js
@@ -11,8 +11,12 @@ const Falcons = () => {
 
     return (
          <div className='falcon-wrap'>
-             {launch.map(value => <Falcon mission_name={value.mission_name} launch_year={value.launch_year}
-                 mission_patch_small={value.links.mission_patch_small} />)}
+             {launch.map(value => {
+                 if (value.launch_year !== '2020') {
+                    return (<Falcon mission_name={value.mission_name} launch_year={value.launch_year}
+                    mission_patch_small={value.links.mission_patch_small} />)}
+             }
+             )}
         </div>
     );
 };
